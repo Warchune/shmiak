699c69541829048f0a48def09d84d638f40dacea
Valera <dinozavr2000@gmail.com>
2019-12-12 23:44:50 +0300

    Аргументы перенесены в командную строку.

--- main.c
+++ main.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
-int main() {
-    float max, step;
-    float v, s;
+int main(int argc, char *argv[]) {
+    float height, step;
+    float v, h;
 
-    scanf("%g%g", &max, &step);
-    s = step;
-    printf("Высота | Скорость\n");
-    while((max - step) >= 0){
-        v = sqrt(2.0 * 10.0 * step);
-        printf("%6g | %g \n", max - step, v);
-        step += s;
+    if (argc < 2 || argc > 3){
+        printf("Error: не верно указаны аргументы.\n%s height [step]\n", argv[0]);
+        return 0;
     }
+
+    if ((height = atoi(argv[1])) < 1){
+        printf("Error: неверно указаны аргументы.\nheigh > step\n");
+        return 0;
+    }
+
+    if (argc == 3) {
+        if ((step = h = atoi(argv[2])) < 1) {
+            printf("Error: неверно указан аргумент.\n%s step\n", argv[0]);
+            return 0;
+        }
+        if (height < step){
+            return 0;
+        }
+    }
+    else {
+        step = h = height / 10;
+    }
+
+    printf("Высота | Скорость\n");
+    while((height - h) >= 0){
+        v = sqrt(2.0 * 10.0 * h);
+        printf("%6.2f | %6.2f \n", height - h, v);
+        h += step;
+    }
+
     return 0;
 }
