d7a29d944ca50c789ee3697f8dbc6ce9e767f08d
Valera <dinozavr2000@gmail.com>
2019-12-19 00:20:06 +0300

    добавлена опция -h, --help

--- main.c
+++ main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 
+struct globalArgs_t{
+    char help;
+} globalArgs;
+
+static const char *optString = "Il:o:vh?";
+
+static const struct option longOpts[] = {
+        { "help", no_argument, NULL, 'h' },
+        { NULL, no_argument, NULL, 0 }
+};
 int main(int argc, char *argv[]) {
+    int opt = 0;
+    int longIndex = 0;
+
+    opt = getopt_long( argc, argv, optString, longOpts, &longIndex );
+    while( opt != -1){
+        switch(opt){
+            case 'h':
+            case '?':
+                printf("Нечего думать, иди прыгай!\n");
+                return 0;
+            default:
+                break;
+        }
+        opt = getopt_long( argc, argv, optString, longOpts, &longIndex );
+    }
+
     float height, step;
     float v, h;
 
@@ -12,7 +40,7 @@ int main(int argc, char *argv[]) {
     }
 
     if ((height = atoi(argv[1])) < 1){
-        printf("Error: неверно указаны аргументы.\nheigh > step\n");
+        printf("Error: неверно указаны аргументы.\nheigh > 1\n");
         return 0;
     }
 
